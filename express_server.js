bde8aaa607991222f85ef17fc577b280f49c4ea5
karlyabut <karleisenyabut@gmail.com>
2019-09-02 23:32:47 +0000

    Implemented a function that generates 6 random characters (unused)

--- express_server.js
+++ express_server.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
+function generateRandomString() {
+  let randomStr = "";
+  const alphaNumStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for(let i = 0; i < 6; i++) {
+    randomStr += alphaNumStr.charAt(Math.floor(Math.random() * alphaNumStr.length))
+  }
+  return randomStr;
+}
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -23,4 +34,24 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) =>  {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };  
+  res.render("urls_show", templateVars);
 });
