ed3a347db589f5e093377d814ed9dc0d7899c412
karlyabut <karleisenyabut@gmail.com>
2019-09-03 21:43:19 +0000

    Implemented a Login and Log out function using cookies

--- express_server.js
+++ express_server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const cookieParser = require('cookie-parser');
 const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.set("view engine", "ejs");
 
 const generateRandomString = function() {
@@ -38,12 +40,15 @@ app.get("/hello", (req, res) =>  {
 
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { username: req.cookies["username"], urls: urlDatabase };
+  console.log(req.cookies["username"]);
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { username: req.cookies["username"] };
+  console.log(req.cookies["username"]);
+  res.render("urls_new", templateVars);
 });
 
 // Generate random letters for key and assign the long url as value
@@ -54,9 +59,21 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${key}`);
 });
 
+//Login function
+app.post("/login", (req, res) => {
+  res.cookie("username", req.body.username);
+  res.redirect("/urls");
+});
+
+app.post("/logout", (req, res) => {
+  res.clearCookie("username");
+  res.redirect("/urls");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  console.log("==>", urlDatabase[req.params.shortURL]);
+  let templateVars = { username: req.cookies["username"], shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  // console.log("==>", urlDatabase[req.params.shortURL]);
+  console.log(req.cookies["username"]);
   res.render("urls_show", templateVars);
 });
 
